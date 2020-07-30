83f4a094afaba5b43878e3dfae3a3991758a6c78
Gabriel Rojas <35940919+gabo2192@users.noreply.github.com>
2020-07-30 18:13:11 -0500

    feat: updated graphql schema

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,12 +1,69 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  enum PersonIdType {
+    DNI
+    CE
+    PASSPORT
+  }
+
+  enum CompanyIdType {
+    RUC
+    RIF
+    RUT
+    NIT
+  }
+
+  type Address {
+    address: String
+    city: String
+    country: String
+    zipCode: String
+  }
+
+  type PersonId {
+    type: PersonIdType
+    number: Int
+  }
+  type CompanyId {
+    type: PersonIdType
+    number: Int
+  }
+
+  type Company {
+    companyName: String
+    companySlogan: String
+    companyEmail: String
+    companyAddress: Address
+    companyId: CompanyId
+    companyRating: Float
+  }
+
   type User {
     _id: ID!
     email: String!
     firstName: String!
     lastName: String!
     role: String
+    identification: PersonId
+    address: Address
+    phone: String
+    linkedin: String
+    company: Company
+    rating: Int
+  }
+
+  input UserInput {
+    email: String
+    firstName: String
+    lastName: String
+    role: String
+    identification: PersonId
+    address: Address
+    phone: String
+    linkedin: String
+    company: Company
+    rating: Int
   }
 
   type AuthData {
